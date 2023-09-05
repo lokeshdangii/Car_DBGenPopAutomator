a850d46c761447fc69b704e9cb6d1911df62e29c
lokeshdangii <lokeshdangi1045@gmail.com>
2023-09-05 19:39:11 +0530

    Script to create and populate Installment table

--- Installment.py
+++ Installment.py
@@ -13,8 +13,8 @@ def create_installment(db):
             InstallmentID INT AUTO_INCREMENT PRIMARY KEY,
             InstallmentNumber INT,
             DueDate DATE,
-            InstallmentAmount DECIMAL(10, 2),
-            RemainingAmount DECIMAL(10, 2),
+            InstallmentAmount INT,
+            RemainingAmount INT,
             TotalInstallment INT
         )
     """)
@@ -23,8 +23,8 @@ def create_installment(db):
     installment_data = []
     for i in range(1, 101):
         installment_data.append((None, fake.random_int(min=1, max=12), fake.future_date(end_date='+1y'), 
-                                fake.pydecimal(left_digits=5, right_digits=2), 
-                                fake.pydecimal(left_digits=5, right_digits=2), 
+                                fake.random_int(min=15000,max=50000), 
+                                fake.random_int(min=300000,max=1200000), 
                                 fake.random_int(min=12, max=60)))
     insert_installment_query = "INSERT INTO Installment (InstallmentID, InstallmentNumber, DueDate, InstallmentAmount, RemainingAmount, TotalInstallment) VALUES (%s, %s, %s, %s, %s, %s)"
     cursor.executemany(insert_installment_query, installment_data)
