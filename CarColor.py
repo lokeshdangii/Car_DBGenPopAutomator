9e710753588ebcb8377e6582d6dbc109e30c2d8e
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-15 22:59:12 +0530

    Script to create and populate car color table

--- CarColor.py
+++ CarColor.py
@@ -5,32 +5,29 @@ def create_car_color(db):
     cursor = db.cursor()
 
     car_colors = [
-        (1, "White"),
-        (2, "Black"),
-        (3, "Silver"),
-        (4, "Gray"),
-        (5, "Red"),
-        (6, "Blue"),
-        (7, "Green"),
-        (8, "Brown"),
-        (9, "Yellow"),
-        (10, "Orange")
+        ("White"),
+        ("Black"),
+        ("Silver"),
+        ("Gray"),
+        ("Red"),
+        ("Blue"),
+        ("Green"),
+        ("Brown"),
+        ("Yellow"),
+        ("Orange")
     ]
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS CarColor (
-            ColorID INT PRIMARY KEY,
+            ColorID INT AUTO_INCREMENT PRIMARY KEY,
             ColorName VARCHAR(50)
         )
     """)
 
-    insert_query = "INSERT INTO CarColor (ColorID, ColorName) VALUES (%s, %s)"
+    insert_query = "INSERT INTO CarColor (ColorName) VALUES (%s)"
     cursor.executemany(insert_query, car_colors)
 
     db.commit()
     cursor.close()
    
     print("CarColor table created and populated successfully.")
-
-
-
